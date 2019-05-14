f92ccc34f149f42a5dc9a9561eedb3a45a312448
Priscilla Okawa <priscilla.okawa@ibm.com>
2019-05-13 18:39:17 -0700

    setup TDD for input file 1

---
+++ test/inputFileOneTests.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const expect = require("chai").expect;
+const ShoppingListSpaceCase = require("../src/inputFileOne").expect;
+
+describe("Shopping list with spaces case", () => {
+  let shoppingListSpaceCase;
+
+  beforeEach(() => {
+    shoppingListSpaceCase = new ShoppingListSpaceCase();
+  });
+
+  afterEach(() => {
+    shoppingListSpaceCase = undefined;
+  });
+
+  it("ShoppingListSpaceCase should be an object", () => {
+    expect(shoppingListSpaceCase).to.be.a("object");
+  });
+});
